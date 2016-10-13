f4015adefdfd192f745e554f3914a5fb34898228
Badder <badder.marzocchi@cern.ch>
2016-10-13 15:34:57 +0200

    launch T2_Rome

---
+++ scripts/launchRun.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+#python submitBatch_T2Rome.py -r 2517,2518,2519,2520,2521,2522,2523,2524,2525,2526,2527,2528,2529,2386,2387,2388,2389,2390,2391,2392,2393,2394,2395,2396,2397,2398,2399,2401,2402,2403,2404,2405,2406,2407,2408,2426,2427,2428,2429,2430,2431 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_MiB_25m.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2391,2392,2393,2394,2395,2396,2397,2398,2399,2401,2402,2403,2404,2405,2406,2407,2408,2426,2427,2428,2429,2430,2431 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_MiB_25m.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2409,2410,2411,2412,2413,2414,2415,2416,2417,2418,2419,2420,2421,2422,2423,2424,2425,2432,2433,2434,2435,2436 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_MiB_10m.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2470,2471,2472,2473,2474,2475,2476,2477,2478,2479,2480,2481,2482,2483,2484,2485,2486,2487,2488,2489,2490,2491,2492,2493 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_Rm_5m.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2465,2466,2467,2468,2469,2455,2456,2457,2458,2459,2460,2461,2462,2463,2464,2494,2495,2496,2497,2498,2499,2500,2501,2502 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_Rm_8m.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2328,2329,2330,2331,2332,2333,2336,2338,2339,2340 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_RU1.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2341,2342,2343,2344,2345,2346,2347,2348,2349,2350,2351  -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_RU2.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2352,2353,2354,2355,2356,2357,2358,2359,2360,2361,2362 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_RU3.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2363,2364,2365,2366,2367,2368,2369,2370,2371 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_RU4.cfg --batch lxbatch
+
+python submitBatch_T2Rome.py -r 2384 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_RU5.cfg --batch lxbatch
+
+#python submitBatch_T2Rome.py -r 2547,2549,2550,2551,2552,2553,2554,2555,2556,2557,2558,2559,2560,2561 -q cmsan -s root://eoscms.cern.ch//store/group/dpg_ecal/comm_ecal/upgrade/testbeam/TimingTB_BTF_Jun2016/ntuples/v1/ -c ../cfg/BTF2016_RU6.cfg --batch lxbatch
---
+++ scripts/submitBatch_T2Rome.py
@@ -0,0 +1,124 @@
+#!/usr/bin/python2
+
+import sys
+import os
+import commands
+from commands import getstatusoutput
+from commands import getoutput
+import datetime
+import argparse
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+class customAction(argparse.Action):
+    def __call__(self, parser, args, values, option_string=None):
+        setattr(args, self.dest, values.split(','))
+
+def get_comma_separated_args(self, arg_line):
+    return arg_line.split(',')
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+def lxbatchSubmitJob (run, path, cfg, outdir, queue, job_dir, dryrun):
+    jobname = job_dir+'/H4Reco_'+queue+'_'+run+'.sh'
+    f = open (jobname, 'w')
+    f.write ('#!/bin/sh' + '\n\n')
+    f.write ('export X509_USER_PROXY=/cmshome/marzocchi/x509up_u28021 \n\n')
+#    f.write ('git clone --recursive https://github.com/simonepigazzini/H4Analysis.git \n')
+#    f.write ('cd H4Analysis/ \n')
+#    f.write ('cp /cmshome/marzocchi/H4Analysis/cfg/*.cfg cfg/ \n')
+    f.write ('cd ../.. \n') 
+    f.write ('source scripts/setup.sh \n')
+    f.write ('make -j 2 \n')
+#    f.write ('cp '+path+cfg+' job.cfg \n\n')
+#    f.write ('cp '+path+'/ntuples/Template*.root ./ntuples/ \n\n')
+    f.write ('bin/H4Reco '+path+cfg+' '+run+'\n\n')
+    f.write ('cd /cmshome/marzocchi/CMSSW_8_0_8_patch1/src/ \n')
+    f.write ('eval `scramv1 runtime -sh` \n')
+    f.write ('cd /cmshome/marzocchi/H4Analysis/ntuples/ \n')
+    f.write ('ls \n')
+    if "store/" in outdir:
+        f.write ('xrdcp -f *'+run+'.root '+outdir+'\n')
+    else:
+        f.write ('cp ntuples/*'+run+'.root '+outdir+'\n')
+    f.close ()
+    getstatusoutput ('chmod 755 ' + jobname)
+    if not dryrun:
+        getstatusoutput ('cd '+job_dir+'; bsub -q ' + queue + ' ' + '-u badder.marzocchi@cern.ch ' + jobname + '; cd -')
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+def herculesSubmitJob (run, path, cfg, outdir, queue, job_dir, dryrun):
+    jobname = job_dir+'/H4Reco_'+queue+'_'+run+'.sh'
+    f = open (jobname, 'w')
+    f.write ('#!/bin/sh' + '\n\n')
+    f.write ('mkdir -p /gwpool/users/$USER/pool/'+run+'/ \n')
+    f.write ('cd /gwpool/users/$USER/pool/'+run+'/ \n')
+    f.write ('wget https://github.com/simonepigazzini/H4Analysis/archive/master.zip \n')
+    f.write ('unzip master.zip \n')
+    f.write ('cd H4Analysis-master/ \n\n')
+    f.write ('cp '+path+'/ntuples/Template*.root ./ntuples/ \n')
+    f.write ('cp '+path+cfg+' job.cfg \n')
+    f.write ('source scripts/setup.sh \n')
+    f.write ('make -j \n\n')
+    f.write ('bin/H4Reco job.cfg '+run+'\n\n')
+    f.write ('cp ntuples/*'+run+'.root '+outdir+'\n')
+    f.write ('cd /gwpool/users/$USER/pool/ \n')
+    f.write ('rm -r '+run+' \n')
+    f.close ()
+    getstatusoutput ('chmod 755 ' + jobname)
+    if not dryrun:
+        getstatusoutput ('cd '+job_dir+'; qsub -q ' + queue + ' ' + jobname + '; cd -')
+
+# ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ---- ----
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser (description = 'submit H4Reco step to lsf')
+    parser.add_argument('-r', '--runs' , action=customAction, help='run to be processed, either list or file')
+    parser.add_argument('-q', '--queue' , default = '1nh', help='batch queue (1nh)')
+    parser.add_argument('-s', '--storage' , default = '/store/group/dpg_ecal/alca_ecalcalib/ECALTB_H4_Fall2015/', help='storage path')
+    parser.add_argument('-v', '--version' , default = 'v1', help='production version')
+    parser.add_argument('-c', '--cfg' , default = '../cfg/H4DAQ_base.cfg', help='production version')
+    parser.add_argument('--dryrun' , action="store_true", default=False, help='do not submit the jobs, just create them')
+    parser.add_argument('--batch' , default='lxbatch', help='batch system to use')
+    
+    args = parser.parse_args ()
+
+    ## check ntuple version
+    stageOutDir = args.storage+'ntuples_'+args.version+'/'
+    
+    if args.batch == 'lxbatch':
+        if getoutput('gfal-ls root://eoscms/'+stageOutDir) == "":
+            print "ntuples version "+args.version+" directory on eos already exist! no jobs created."
+            #exit(0)
+        getstatusoutput('cmsMkdir '+stageOutDir)    
+    else:
+        getstatusoutput('mkdir -p '+stageOutDir)    
+    
+    ## job setup
+    local_path = getoutput('pwd')
+    date = datetime.datetime.now().strftime("%d-%m-%Y")
+    job_dir = local_path+"/"+date+"_ntuples_"+args.version
+    getstatusoutput('mkdir -p '+job_dir)
+    if local_path.endswith('scripts'):
+        local_path = local_path[:-len('scripts')]
+
+    if args.cfg.startswith('../'):
+        args.cfg = args.cfg[len('../'):]
+    
+    if len(args.runs) == 1 and os.path.isfile(args.runs[0]):
+        runs_file = open(args.runs[0], 'r')
+        args.runs  = []
+        if runs_file:
+            for run in runs_file:
+                args.runs.append(run.rstrip())
+
+    ## create jobs
+    print 'submitting jobs to queue', args.queue
+    for run in args.runs:
+        print 'submitting run: ', run
+        if args.batch == 'lxbatch':
+            lxbatchSubmitJob(run, local_path, args.cfg, stageOutDir, args.queue, job_dir, args.dryrun) 
+        if args.batch == 'hercules':
+            herculesSubmitJob(run, local_path, args.cfg, stageOutDir, args.queue, job_dir, args.dryrun) 
